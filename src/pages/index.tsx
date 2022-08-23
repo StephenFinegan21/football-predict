15927a8bf0f703dc198919feebf458bd7225b764
StephenFinegan21 <x20191952@student.ncirl.com>
2022-08-23 22:56:16 +0100

    g analytics tracking

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import ListContainer from "../components/ListContainer";
 import axios from "axios";
+import Script from "next/script";
 
 const Home: NextPage = ({ data }: any) => {
 
@@ -15,6 +16,20 @@ const Home: NextPage = ({ data }: any) => {
         />
         <link rel="icon" href="/football-icon.jpeg" />
       </Head>
+      <Script 
+        
+        src={`https://www.googletagmanager.com/gtag/js?id${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        strategy={"afterInteractive"} 
+        />
+        <Script id={"google-analytics"} strategy={"afterInteractive"}>
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+        `}
+        </Script>
       <ListContainer fixtures={data} />
     </>
   );
